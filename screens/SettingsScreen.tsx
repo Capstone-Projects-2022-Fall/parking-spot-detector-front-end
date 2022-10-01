ea280a33c5f321eda303b1c0f1e74e11d129aa82
Steve Tolvaj <stevetolvaj@gmail.com>
2022-10-01 14:08:27 -0400

    feature: API hello world and styling updated -Added logo -Added button to retrieve test data from api -Will need to change url and interface to retrieve data from remote

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,14 +1,60 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, Image } from "react-native";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { User } from "../interfaces";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+import React from "react";
 
-export default function TabTwoScreen() {
+export default function SettingsScreen() {
+  const [userData, setUserData] = useState<User[]>([]);
+
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  console.log("User data ", userData);
+
+  const update = () => {
+    axios
+      .get<User[]>("http://10.0.2.2:8080/parking/user")
+      .then((response: AxiosResponse) => {
+        console.log("Response ", response.data);
+        setUserData(response.data);
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get<User[]>("http://10.0.2.2:8080/parking/user")
+      .then((response: AxiosResponse) => {
+        console.log("Response ", response.data);
+        setUserData(response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>
+        Settings currently used for testing get API request:
+      </Text>
+      <Button
+        title="Click to fetch data"
+        onPress={() => {
+          update();
+          setButtonClicked(true);
+        }}
+      />
+      <View style={styles.container}>
+        {buttonClicked == true ? <Text>{JSON.stringify(userData)}</Text> : null}
+
+        {buttonClicked == true ? (
+          <Image
+            source={{
+              uri: "http://10.0.2.2:8080/parking/image",
+            }}
+            style={{ width: 300, height: 300, marginTop: 20 }}
+          />
+        ) : null}
+      </View>
     </View>
   );
 }
@@ -16,16 +62,17 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
+    justifyContent: "center",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
