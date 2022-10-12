8974ed4ed8a7abdd40e5a8f92ad2515e5857f58f
yasminewatkins <yasminepwatkins@gmail.com>
2022-10-12 17:15:42 -0400

    feature: registration funcionality updated -added states for input -added dispach for sending data to register user thunk -added navigation and clearing states after registration btn is pressed

--- screens/login/Register.tsx
+++ screens/login/Register.tsx
@@ -1,165 +1,100 @@
 import { Text, View } from "../../components/Themed";
-import { useNavigation } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  TouchableHighlight,
-  Platform,
-} from "react-native";
-import { Button } from "native-base";
-import { useAppDispatch } from "../../hooks/hooks";
-import { registerUser } from "../../redux/user/userSlice";
-import { User } from "../../redux/user/index";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TextInput } from "react-native";
+import { Button, Center } from "native-base";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { logoutUser, registerUserThunk } from "../../redux/user/userSlice";
+import { initialState, LoginStatus } from "../../redux/user/index";
+import { StackActions, useNavigation } from "@react-navigation/native";
 
 export default function RegisterScreen() {
-  var user: User = {
-    userId: 0,
-    userName: "testName",
-    email: "testMail",
-    password: "testPass",
-    phone: 5555555555,
-  };
+  const [userFirstName, setFirstName] = useState("");
+  const [userLastName, setLastName] = useState("");
 
-  const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [userPass, setUserPass] = useState("");
-  const [userPhone, setUserPhone] = useState(0);
 
-  var dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation();
+  let data = { ...initialState };
+
+  useEffect(() => {
+    console.log("LoginStatus: " + user.status);
+
+    if (user.regStatus == LoginStatus.SUCCEEDED) {
+      // Clear input text states.
+      setFirstName("");
+      setLastName("");
+      setUserPass("");
+      // Logout user to start with fresh state.
+      dispatch(logoutUser());
+      alert("Successful Registration. Sign in to continue");
+      navigation.dispatch(StackActions.replace("SignIn"));
+    }
+  }, [user.regStatus]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Parking Spot Detector</Text>
+      <Center w="100%">
+        <Text style={styles.title}>
+          {" "}
+          Parking Spot Detector{"\n"}Registration
+        </Text>
 
-      <Image
-        style={styles.image}
-        source={require("../../assets/images/parking_logo.png")}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        autoCapitalize="none"
-        placeholderTextColor="white"
-        onChangeText={(val) => setUserName(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        autoCapitalize="none"
-        placeholderTextColor="white"
-        onChangeText={(val) => setUserEmail(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        autoCapitalize="none"
-        placeholderTextColor="white"
-        onChangeText={(val) => setUserPass(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Phone Number"
-        autoCapitalize="none"
-        placeholderTextColor="white"
-        onChangeText={(val) => setUserPhone(Number(val))}
-      />
-      <Button
-        style={styles.regBtn}
-        onPress={() => {
-          dispatch(registerUser(user));
-        }}
-      >
-        {" "}
-        Register{" "}
-      </Button>
+        <TextInput
+          style={styles.input}
+          placeholder="First name"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setFirstName(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Last name"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setLastName(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setUserEmail(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(val) => setUserPass(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(val) => setUserPass(val)}
+        />
+        <Button
+          style={styles.regBtn}
+          onPress={() => {
+            data.name = userFirstName + userLastName;
+            data.email = userEmail;
+            data.username = userPass;
+
+            dispatch(registerUserThunk(data));
+          }}
+        >
+          Register
+        </Button>
+      </Center>
     </View>
   );
 }
-
-// export default class SignUp extends React.Component {
-//   state = {
-//     username: "",
-//     password: "",
-//     email: "",
-//     phone_number: "",
-//   };
-//   onChangeText = (key: string, val: string) => {
-//     this.setState({ [key]: val });
-//   };
-//   signUp = async () => {
-//     const { username, password, email, phone_number } = this.state;
-//     try {
-//       // here place your signup logic
-//       console.log("user successfully signed up!: ");
-//     } catch (err) {
-//       console.log("error signing up: ", err);
-//     }
-//   };
-
-//   render() {
-//     var user: User = {
-//       userName: "testName",
-//       email: "testMail",
-//       password: "testPass",
-//       phone: 5555555555,
-//     };
-
-//     var dispatch = useAppDispatch();
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.title}> Parking Spot Detector</Text>
-
-//         <Image
-//           style={styles.image}
-//           source={require("../../assets/images/parking_logo.png")}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Username"
-//           autoCapitalize="none"
-//           placeholderTextColor="white"
-//           onChangeText={(val) => this.onChangeText("username", val)}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Password"
-//           secureTextEntry={true}
-//           autoCapitalize="none"
-//           placeholderTextColor="white"
-//           onChangeText={(val) => this.onChangeText("password", val)}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Email"
-//           autoCapitalize="none"
-//           placeholderTextColor="white"
-//           onChangeText={(val) => this.onChangeText("email", val)}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Phone Number"
-//           autoCapitalize="none"
-//           placeholderTextColor="white"
-//           onChangeText={(val) => this.onChangeText("phone_number", val)}
-//         />
-//         <Button
-//           style={styles.regBtn}
-//           onPress={() => {
-//             dispatch(registerUser(user));
-//             this.signUp;
-//           }}
-//         >
-//           {" "}
-//           Register{" "}
-//         </Button>
-//       </View>
-//     );
-//   }
-// }
 
 const styles = StyleSheet.create({
   input: {
@@ -191,6 +126,7 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 30,
+    textAlign: "center",
   },
 
   image: {
