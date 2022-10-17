f8565dd823e08e12c45d327b4c4e6624c1a037cc
Steve Tolvaj <stevetolvaj@gmail.com>
2022-10-17 08:14:55 -0400

    fix: styling update

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -40,166 +40,169 @@ export default function ProfileScreen({
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Update Profile</Text>
-      <HStack alignItems="center" space={4}>
-        <Text>Handicap?</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isEnabled ? "#7059A3" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
+      <Center w="100%">
+        <Text style={styles.title}>Update Profile</Text>
+        <HStack alignItems="center" space={4}>
+          <Text>Handicap?</Text>
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#7059A3" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </HStack>
+        <TextInput
+          style={styles.input}
+          placeholder="First name"
+          value={userFirstName}
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setFirstName(val)}
         />
-      </HStack>
-      <TextInput
-        style={styles.input}
-        placeholder="First name"
-        value={userFirstName}
-        autoCapitalize="none"
-        placeholderTextColor="#003f5c"
-        onChangeText={(val) => setFirstName(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Last name"
-        value={userLastName}
-        autoCapitalize="none"
-        placeholderTextColor="#003f5c"
-        onChangeText={(val) => setLastName(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        textContentType="emailAddress"
-        value={userEmail}
-        autoCapitalize="none"
-        placeholderTextColor="#003f5c"
-        onChangeText={(val) => setUserEmail(val)}
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="Last name"
+          value={userLastName}
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setLastName(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          textContentType="emailAddress"
+          value={userEmail}
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setUserEmail(val)}
+        />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Phone number"
-        textContentType="telephoneNumber"
-        dataDetectorTypes="phoneNumber"
-        keyboardType="phone-pad"
-        maxLength={14}
-        value={formatPhoneNumber(userPhone)}
-        placeholderTextColor="#003f5c"
-        onChangeText={(val) => setUserPhone(formatPhoneNumber(val))}
-      />
-      <GooglePlacesAutocomplete
-        styles={{
-          container: {
-            marginTop: 10,
-            flex: 0,
-          },
-          textInputContainer: {
-            width: "70%",
-            backgroundColor: "#C19FDE",
-            borderRadius: 30,
-          },
-          textInput: {
-            borderRadius: 30,
-            backgroundColor: "#C19FDE",
-            height: 36,
-            fontSize: 17,
-            fontWeight: "600",
-          },
-        }}
-        placeholder={locationField}
-        textInputProps={{
-          placeholderTextColor: "#003f5c",
-          returnKeyType: "search",
-        }}
-        query={{
-          key: API_KEY,
-          language: "en", // language of the results
-        }}
-        onPress={(data, details = null) => {
-          setUserAddress(data.description);
-          console.log(details);
-        }}
-        onFail={(error) => console.error(error)}
-      />
-      <Text>*Leave passwords empty if not updating*</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        autoCapitalize="none"
-        placeholderTextColor="#003f5c"
-        secureTextEntry={true}
-        onChangeText={(val) => setUserPass1(val)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        autoCapitalize="none"
-        placeholderTextColor="#003f5c"
-        secureTextEntry={true}
-        onChangeText={(val) => setUserPass2(val)}
-      />
-      <View style={styles.button}>
-        <TouchableOpacity
-          style={styles.logoutBtn}
-          onPress={() => {
-            if (
-              userPass1 != "" &&
-              userPass2 != "" &&
-              userPass1 == userPass2 &&
-              userPass1.length < 8
-            ) {
-              alert(
-                "Password must be 8 characters minimum or empty if not updating"
-              );
-            } else if (userPass1 != userPass2) {
-              alert("Passwords do not match");
-            } else if (userEmail.length < 1) {
-              alert("Invalid email address");
-            } else if (userFirstName.length < 1 || userLastName.length < 1) {
-              alert("Name field missing");
-            } else if (userAddress.length < 1) {
-              alert("Invalid address");
-            } else if (userPhone.length < 10) {
-              alert("Invalid phone number");
-            } else {
-              data.first_name = userFirstName;
-              data.last_name = userLastName;
-              data.address = userAddress;
-              data.phone_number = userPhone;
-              data.handicap = isEnabled;
-              data.email = userEmail;
-              data.password_hash = userPass1;
-              // Update in server, if password is supplied it will be hashed in the thunk.
-              dispatch(updateUserProfileThunk(data));
-            }
+        <TextInput
+          style={styles.input}
+          placeholder="Phone number"
+          textContentType="telephoneNumber"
+          dataDetectorTypes="phoneNumber"
+          keyboardType="phone-pad"
+          maxLength={14}
+          value={formatPhoneNumber(userPhone)}
+          placeholderTextColor="#003f5c"
+          onChangeText={(val) => setUserPhone(formatPhoneNumber(val))}
+        />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              marginTop: 10,
+              marginBottom: 5,
+              flex: 0,
+            },
+            textInputContainer: {
+              width: "70%",
+              backgroundColor: "#C19FDE",
+              borderRadius: 30,
+            },
+            textInput: {
+              borderRadius: 30,
+              backgroundColor: "#C19FDE",
+              height: 36,
+              fontSize: 17,
+              fontWeight: "600",
+            },
           }}
-        >
-          <Text>Update</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.logoutBtn}
-          onPress={() => {
-            dispatch(logoutUser());
-            navigation.dispatch(StackActions.replace("SignIn"));
+          placeholder={locationField}
+          textInputProps={{
+            placeholderTextColor: "#003f5c",
+            returnKeyType: "search",
           }}
-        >
-          <Text>Logout</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.deleteBtn}
-          onPress={() => {
-            // Delete user profile on server.
-            dispatch(deleteUserThunk(user));
-            // Logout to start with initial state.
-            dispatch(logoutUser());
-            // Return to sign in screen.
-            navigation.dispatch(StackActions.replace("SignIn"));
+          query={{
+            key: API_KEY,
+            language: "en", // language of the results
           }}
-        >
-          <Text>Delete</Text>
-        </TouchableOpacity>
-      </View>
+          onPress={(data, details = null) => {
+            setUserAddress(data.description);
+            console.log(details);
+          }}
+          onFail={(error) => console.error(error)}
+        />
+        <Text>*Leave passwords empty if not updating*</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(val) => setUserPass1(val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          autoCapitalize="none"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(val) => setUserPass2(val)}
+        />
+        <View style={styles.button}>
+          <TouchableOpacity
+            style={styles.logoutBtn}
+            onPress={() => {
+              if (
+                userPass1 != "" &&
+                userPass2 != "" &&
+                userPass1 == userPass2 &&
+                userPass1.length < 8
+              ) {
+                alert(
+                  "Password must be 8 characters minimum or empty if not updating"
+                );
+              } else if (userPass1 != userPass2) {
+                alert("Passwords do not match");
+              } else if (userEmail.length < 1) {
+                alert("Invalid email address");
+              } else if (userFirstName.length < 1 || userLastName.length < 1) {
+                alert("Name field missing");
+              } else if (userAddress.length < 1) {
+                alert("Invalid address");
+              } else if (userPhone.length < 10) {
+                alert("Invalid phone number");
+              } else {
+                data.first_name = userFirstName;
+                data.last_name = userLastName;
+                data.address = userAddress;
+                data.phone_number = userPhone;
+                data.handicap = isEnabled;
+                data.email = userEmail;
+                data.password_hash = userPass1;
+                // Update in server, if password is supplied it will be hashed in the thunk.
+                dispatch(updateUserProfileThunk(data));
+              }
+            }}
+          >
+            <Text>Update</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.logoutBtn}
+            onPress={() => {
+              dispatch(logoutUser());
+              navigation.dispatch(StackActions.replace("SignIn"));
+            }}
+          >
+            <Text>Logout</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.deleteBtn}
+            onPress={() => {
+              // Delete user profile on server.
+              dispatch(deleteUserThunk(user));
+              // Logout to start with initial state.
+              dispatch(logoutUser());
+              // Return to sign in screen.
+              navigation.dispatch(StackActions.replace("SignIn"));
+            }}
+          >
+            <Text>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      </Center>
     </View>
   );
 }
@@ -207,11 +210,12 @@ export default function ProfileScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    marginBottom: 50,
   },
   button: {
     flex: 1,
+    marginTop: 10,
     flexDirection: "row",
     justifyContent: "space-between",
   },
@@ -245,13 +249,8 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 25,
-    marginTop: 20,
+
     padding: 0,
     textAlign: "center",
-  },
-
-  image: {
-    width: "50%",
-    resizeMode: "contain",
   },
 });
