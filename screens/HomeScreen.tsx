12c03f1ae0207bc09a48e54c4900adde6f85fc13
Steve Tolvaj <stevetolvaj@gmail.com>
2022-10-22 13:23:38 -0400

    feature: added push notification data -currently on home screen for testing

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,11 +4,60 @@ import { formatPhoneNumber } from "../constants/Formatters";
 import { useAppSelector } from "../hooks/hooks";
 import { RootTabScreenProps } from "../types";
 
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Platform } from "react-native";
+import { Subscription } from "expo-modules-core";
+
+// ExponentPushToken[X7REOHMNL5IcMAkMS71A8v] on my device for testing
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function HomeScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
   const user = useAppSelector((state) => state.user);
   console.log(JSON.stringify(user));
+
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>("");
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+  const notificationListener = useRef<Subscription>();
+  const responseListener = useRef<Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, [notification]);
 
   return (
     <View style={styles.container}>
@@ -20,9 +69,22 @@ export default function HomeScreen({
         {"\n"}Handicap status: {String(user.handicap)}
         {"\n"}LoginStatus: {user.status}
       </Text>
-      <Image
-        source={require("../assets/images/parking_spot_logo.png")}
-        style={{ width: "80%", resizeMode: "contain" }}
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
       />
       <View
         style={styles.separator}
@@ -34,6 +96,61 @@ export default function HomeScreen({
       {/* <EditScreenInfo path="/screens/HomeScreen.tsx" /> */}
     </View>
   );
+}
+
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
+async function sendPushNotification(expoPushToken: string | undefined) {
+  const message = {
+    to: expoPushToken,
+
+    sound: "default",
+    title: "Original Title",
+    body: "And here is the body!",
+    data: { someData: "goes here, maybe parking/camera data" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token = "";
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
 }
 
 const styles = StyleSheet.create({
