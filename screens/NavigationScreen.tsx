c74f4d37e0fe6967d79238a233597b702ac0af9b
Steve Tolvaj <stevetolvaj@gmail.com>
2022-11-13 09:22:31 -0500

    fix: removed custom parking marker -Too large on IOS -Animate to region was not updating

--- screens/NavigationScreen.tsx
+++ screens/NavigationScreen.tsx
@@ -12,7 +12,7 @@ import { useAppSelector } from "../hooks/hooks";
 /**
  * The NavigationScreen is used to show the map of the users current location and the nearest parking available from the
  * push notification. It also allows the user to navigate to the nearest open parking location if available and opens
- * directions in google maps.
+ * directions in google maps. Icon removed and view centers if false is recieved from parking.aval.
  * @param RootTabScreenProps The root tab props needed for the bottom tabs navigation.
  * @returns The view of the navigation screen.
  */
@@ -20,15 +20,15 @@ export default function NavigationScreen({
   navigation,
 }: RootTabScreenProps<"TabThree">) {
   const [location, setLocation] = useState<Location.LocationObject>();
-  const [delta, setDelta] = useState([0.004, 0.007]);
+  const [delta, setDelta] = useState([0.004, 0.007]); // May be used to change zoom buffer around markers on map.
 
-  // TODO use this to update marker on map and change lat and long delta to match distance between them.
-  const parking = useAppSelector((state) => state.parking);
+  const parking = useAppSelector((state) => state.parking); // The parking state object updated from notification or background click event.
 
   const mapRef = React.createRef<MapView>();
-  const markerRef = React.createRef<Marker>();
-  const parkingMarkerRef = React.createRef<Marker>();
+  const markerRef = React.createRef<Marker>(); // Current user location marker (car).
+  const parkingMarkerRef = React.createRef<Marker>(); // Parking marker if available, transparent if not.
 
+  // Initial map region before location is updated.
   const [mapRegion, setmapRegion] = useState({
     latitude: 39.9826281,
     longitude: -75.1629567,
@@ -44,17 +44,21 @@ export default function NavigationScreen({
         timeInterval: 1500,
       });
 
+      // The initial map view for current region before location is updated (just in case).
       mapRef.current?.animateToRegion(mapRegion);
 
+      // The current user location marker.
       markerRef.current?.animateMarkerToCoordinate({
         latitude: Number(location?.coords.latitude),
         longitude: Number(location?.coords.longitude),
       });
 
       setmapRegion({
+        // The current user location to center the map on.
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
 
+        // Create enough of a zoom buffer to show both user location and parking location * extra (extra is delta use state).
         latitudeDelta: parking.parkingAval
           ? (Math.max(parking.coordinates[0], location.coords.latitude) -
               Math.min(parking.coordinates[0], location.coords.latitude)) *
@@ -69,7 +73,7 @@ export default function NavigationScreen({
 
       setLocation(location);
     })();
-  }, [location, parking.parkingAval]);
+  }, [location]); // Update map if location or parking availability changes.
 
   return (
     <View style={styles.container}>
@@ -81,10 +85,10 @@ export default function NavigationScreen({
         ref={mapRef}
       >
         <Marker
-          image={require("../assets/images/car.png")}
+          image={require("../assets/images/car.png")} // current location
           coordinate={{
             latitude: Number(
-              isNaN(Number(location?.coords.latitude))
+              isNaN(Number(location?.coords.latitude)) // if not available set to 0.0 to avoid crash, usually only during initial emulator load and not visible.
                 ? 0.0
                 : location?.coords.latitude
             ),
@@ -98,11 +102,11 @@ export default function NavigationScreen({
         />
         <Marker
           ref={parkingMarkerRef}
-          image={
-            !parking.parkingAval
-              ? require("../assets/images/transparent_small.png")
-              : require("../assets/images/parking_marker_150px.png")
-          }
+          // image={  // Not working as expected: image too large on IOS and animate to region not working
+          //   !parking.parkingAval // marker is transparent if no parking.
+          //     ? require("../assets/images/transparent_small.png")
+          //     : require("../assets/images/parking_marker_150px.png")
+          // }
           coordinate={{
             latitude: parking.coordinates[0],
             longitude: parking.coordinates[1],
